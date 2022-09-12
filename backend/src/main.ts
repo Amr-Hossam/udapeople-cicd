0792d503529e54f4de5f7bf0fc149ae42bef9adf
Amr-Hossam <amrhossamm@gmail.com>
2022-09-12 14:19:34 +0200

    prepare for prometheus

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
-console.log('Testing another-branch')
+// console.log('Testing another-branch')
 
 async function bootstrap() {
   const logger = new AppLogger();
